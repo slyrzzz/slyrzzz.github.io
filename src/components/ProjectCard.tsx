931235b65c88d88145c26bb5c2d51e682da6c5d5
slyrzzz <jarc.01@outlook.com>
2026-04-11 20:49:31 -0500

    fix imagen principal

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -23,6 +23,7 @@ interface ProjectCardProps {
 
 export const ProjectCard: React.FC<ProjectCardProps> = ({
   href,
+  priority,
   images = [],
   title,
   content,
@@ -33,6 +34,7 @@ export const ProjectCard: React.FC<ProjectCardProps> = ({
   return (
     <Column fillWidth gap="m">
       <Carousel
+        priority={priority}
         sizes="(max-width: 960px) 100vw, 960px"
         items={images.map((image) => ({
           slide: image,
